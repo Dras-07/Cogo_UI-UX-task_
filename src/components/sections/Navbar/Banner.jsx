921abf6b9a4b8ee31faccf44afd595cb24ba173b
Dras-07 <anjanays619@gmail.com>
2023-07-09 19:54:22 +0530

    drop-down-menu

--- src/components/sections/Navbar/Banner.jsx
+++ src/components/sections/Navbar/Banner.jsx
@@ -1,20 +1,26 @@
-import React ,{useState} from 'react';
-// import cogoportIcon from 'https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Fcogoport_icon&w=16&q=75';
+import React, { useState } from 'react';
 import './Banner.css';
-import CloseButton from './CloseButton.jsx';
+import CloseButton from './CloseButton';
+
 function Banner() {
-    const [showBanner, setShowBanner] = useState(true);
+  const [showBanner, setShowBanner] = useState(true);
 
   const handleToggleBanner = () => {
-    setShowBanner(!showBanner);
+    setShowBanner(false);
   };
-    return (
-    <div className="banner">
-   <img className="content-icon" src="https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Fcogoport_icon&w=16&q=75" />
-      Limited Time Offer: Get 15% off on all Cogo Assured Rates. <span> Book Today!</span>
-      <CloseButton onClick={handleToggleBanner} />
 
-    </div>
+  return (
+    showBanner && (
+      <div className={`banner ${showBanner ? '' : 'hidden'}`}>
+        <img
+          className="content-icon"
+          src="https://www.cogoport.com/_next/image?url=https%3A%2F%2Fcdn.cogoport.io%2Fcms-prod%2Fcogo_public%2Fvault%2Foriginal%2Fcogoport_icon&w=16&q=75"
+          alt="Cogoport Icon"
+        />
+        Limited Time Offer: Get 15% off on all Cogo Assured Rates. <span>Book Today!</span>
+        <CloseButton onClick={handleToggleBanner} />
+      </div>
+    )
   );
 }
 
